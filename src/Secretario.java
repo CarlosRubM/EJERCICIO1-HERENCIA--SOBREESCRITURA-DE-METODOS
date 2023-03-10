7a0acc4cb2f3510e73e0692074a5e196efb41e2a
CarlosRubM <carlos.rubio9@alu.uclm.es>
2023-03-10 11:55:03 +0100

    Implementacion clase Secretario

--- src/Secretario.java
+++ src/Secretario.java
@@ -1,2 +1,28 @@
-public class Secretario {
+public class Secretario extends Empleado{
+
+    @Override
+    public int getVacaciones() {
+        return super.getVacaciones();
+    }
+
+    @Override
+    public double getSalario() {
+        return super.getSalario()+5000;
+    }
+
+    @Override
+    public int getHoras() {
+        return super.getHoras();
+    }
+
+    @Override
+    public String getMesVacaciones() {
+        return "Agosto";
+    }
+
+    public void mostrar(){
+        System.out.println("Estoy en el tribunal");
+    }
+
+
 }
